54f1785453459f18194f4d1ffad7c946d142043a
David Martinez <47610359+dvdmrtnz@users.noreply.github.com>
2024-01-23 19:30:50 +0100

    Add A#3, B3, C6, C#6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-	notes = ['C4','Cs4','D4','Ds4','E4','F4','Fs4','G4','Gs4','A4','As4','B4',
-			'C5','Cs5','D5','Ds5','E5','F5','Fs5','G5','Gs5','A5','As5','B5'];
+	notes = ['As3','B3','C4','Cs4','D4','Ds4','E4','F4','Fs4','G4','Gs4','A4','As4','B4',
+			'C5','Cs5','D5','Ds5','E5','F5','Fs5','G5','Gs5','A5','As5','B5', 'C6', 'Cs6'];
 
 	for (i in notes)
 	{
